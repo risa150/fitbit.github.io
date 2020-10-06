118f7487524fc0fdf9bccba1f6eedc37a2d3dbf9
risa <b1502363@planet.kanazawa-it.ac.jp>
2020-10-06 23:34:06 +0900

    update

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -23,7 +23,7 @@ xhr.onload = () => {
 xhr.send('');
 const bpms1 = [97,98,99,100,101,102];
 alert(bpms);
-alert(bpms1);
+//alert(bpms1);
 //document.write('bpms     : ' + bpms <br>);
 //document.write('bpm1     : ' + bpm1 );
 
