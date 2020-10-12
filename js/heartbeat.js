b1cccfda9f07027607d4b5f7c8a6ce7e1613e654
risa <b1502363@planet.kanazawa-it.ac.jp>
2020-10-12 15:52:18 +0900

    update

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -15,6 +15,7 @@ var beat = null; //心拍の音を入れる箱
 var data = "67,68,";
 var xhr = new XMLHttpRequest(); 
 xhr.withCredentials = true;
+
 /*fetch('http://54.248.228.235/index.txt')
   .then(response => {
     data = responseText;
